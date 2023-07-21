d9f801d9e64ea21020eedab83a43c5cdc59b550f
Naposetivi <110626741+Naposetivi@users.noreply.github.com>
2023-07-21 11:15:21 +0300

    completed the course program

--- BethanysPieShop.InventoryManagement/Domain/ProductManfgement/Product.cs
+++ BethanysPieShop.InventoryManagement/Domain/ProductManfgement/Product.cs
@@ -1,39 +1,51 @@
-﻿using System;
+﻿using BethanysPieShop.InventoryManagement.Domain.General;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BethanysPieShop.InventoryManagement
+namespace BethanysPieShop.InventoryManagement.Domain.ProductMagament
 {
     public class Product
     {
-        public Product(int id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
 
-        public Product(int id, string name, string? description, UnitType unitType, int maxAmountInStock)
+        public Product(int id, string name, string? description, UnitType unitType, int maxAmountInStock, Price price)
         {
             Id = id;
             Name = name;
             Description = description;
             UnitType = unitType;
+            Price = price;
 
             AmountInStock = maxAmountInStock;
 
             UpdateLowStock();
         }
 
+        public static int StockThreshold = 5;
+
+        public static void ChangeStockThreshold(int newStockThreshold)
+        {
+            // we will only allow this to go through if the value is > 0
+            if (newStockThreshold < 0) { 
+                StockThreshold = newStockThreshold;
+            }
+        }
+
+        #region Fields
         private int id;
         private string name = string.Empty;
         private string? description;
 
         private int maxItemsInStock = 0;
+        #endregion
 
+        #region Properties
         public int Id { get; set; }
-        public string Name { get
+        public string Name
+        {
+            get
             {
                 return name;
             }
@@ -42,11 +54,14 @@ namespace BethanysPieShop.InventoryManagement
                 name = value.Length > 50 ? value[..50] : value;
             }
         }
-        public string? Description { 
-            get {
+        public string? Description
+        {
+            get
+            {
                 return description;
-            } 
-            set {
+            }
+            set
+            {
                 if (value == null)
                 {
                     description = string.Empty;
@@ -55,15 +70,18 @@ namespace BethanysPieShop.InventoryManagement
                 {
                     description = value.Length > 250 ? value[..250] : value;
                 }
-            } }
+            }
+        }
         public UnitType UnitType { get; set; }
         public int AmountInStock { get; private set; }
         public bool IsBelowStockThreshold { get; private set; }
-
-        //using products
+        public Price Price { get; set; }
+        #endregion 
+        //Using products
         public void UseProduct(int items)
         {
-            if (items <= AmountInStock) {
+            if (items <= AmountInStock)
+            {
                 // use the items
                 AmountInStock -= items;
 
@@ -77,15 +95,38 @@ namespace BethanysPieShop.InventoryManagement
             }
         }
 
-        //adding products in stock
+        //Adding product in stock
         public void IncreaseStock()
         {
             AmountInStock++;
         }
 
+
+        //Adding products in stock
+        public void IncreaseStock(int amount)
+        {
+            int newStock = AmountInStock + amount;
+
+            if (newStock <= maxItemsInStock)
+            {
+                AmountInStock += amount;
+            }
+            else
+            {
+                AmountInStock = maxItemsInStock; // we only store the possible items, overstock isn`t stored
+                Log($"{CreateSimpleProductRepresentation} stock overflow. {newStock - AmountInStock} item(s) ordered that couldn`t be stored.");
+            }
+
+            if (AmountInStock > StockThreshold)
+            {
+                IsBelowStockThreshold = false;
+            }
+        }
+
         private void DecreiseStock(int items, string reason)
         {
-            if (items <= AmountInStock) {  
+            if (items <= AmountInStock)
+            {
                 AmountInStock -= items;
             }
             else
@@ -96,6 +137,7 @@ namespace BethanysPieShop.InventoryManagement
             Log(reason);
         }
 
+        //Displaying details of products
         public string DisplayDetailsShort()
         {
             return $"{id}. {name} \n{AmountInStock} items in stock.";
@@ -103,9 +145,27 @@ namespace BethanysPieShop.InventoryManagement
 
         public string DisplayDetailsFull()
         {
+            //StringBuilder sb = new();
+
+            //sb.Append($"{id} {name} \n{description}\n{AmountInStock} item(s) in stock.");
+
+            //if (IsBelowStockThreshold)
+            //{
+            //    sb.Append("\n!!STOCK LOW!!");
+            //}
+
+            //return sb.ToString();
+
+            return DisplayDetailsFull("");
+        }
+
+        public string DisplayDetailsFull(string extraDetails)
+        {
             StringBuilder sb = new();
-            //ToDo: add price too
-            sb.Append($"{id} {name} \n{description}\n{AmountInStock} item(s) in stock.");
+
+            sb.Append($"{id} {name} \n{description}\n{Price}\n{AmountInStock} item(s) in stock.");
+
+            sb.Append(extraDetails);
 
             if (IsBelowStockThreshold)
             {
@@ -115,23 +175,23 @@ namespace BethanysPieShop.InventoryManagement
             return sb.ToString();
         }
 
-        //updating products if their number is too small
+        //Updating products if their number is too small
         private void UpdateLowStock()
         {
-            if (AmountInStock < 10)//for now a fixed value
+            if (AmountInStock < StockThreshold)//for now a fixed value
             {
                 IsBelowStockThreshold = true;
             }
         }
 
-        //changeing Console.WriteLine on Log method
+        //Changeing Console.WriteLine on Log method
         private void Log(string message)
         {
             //this could be written to a file
             Console.WriteLine(message);
         }
 
-        //presentation of product
+        //Presentation of product
         private string CreateSimpleProductRepresentation()
         {
             return $"Product {id} ({name})";
--- BethanysPieShop.InventoryManagement/Program.cs
+++ BethanysPieShop.InventoryManagement/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BethanysPieShop.InventoryManagement.Domain.General;
+using BethanysPieShop.InventoryManagement.Domain.ProductMagament;
+
+Product.ChangeStockThreshold(10);
+
+Price samplePrice = new(10, Currency.Euro);
+Product p1 = new(1, "Sugar", "Lorem ipsum", UnitType.PerKg, 100, samplePrice);
+
+p1.IncreaseStock(10);
+p1.Description = "Sample description";
